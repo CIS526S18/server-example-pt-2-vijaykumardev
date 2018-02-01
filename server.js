61717f3b881dfcb3811d158de7d9fd0b95ac2431
Vijay Kumar <vijaykumardev@users.noreply.github.com>
2018-02-01 14:18:58 -0600

    Asynchronous web service with folder listing
    Uses files from cache to store the web server request and lists all the file name for the root directory

--- server.js
+++ server.js
@@ -9,19 +9,72 @@ const http = require('http');
 // Import the fs library 
 const fs = require('fs');
 
+const cache = {};
+cache['openhouse.html'] = fs.readFileSync('public/openhouse.html')
+cache['openhouse.css'] = fs.readFileSync('public/openhouse.css')
+cache['openhouse.js'] = fs.readFileSync('public/openhouse.js')
+
+/** @function serveIndex
+* Serves the file content of the root directory
+* @param {string} path - specifies the folder path to list the files
+* @param {serverResponse} res - the http response object
+*/
+
+function serveIndex(path,res){
+    fs.readdir(path,(err,files)=>{
+        if(err) {
+            console.error(err)
+            res.statusCode = 500
+            res.end("Server Error")
+        }
+        var html = "<p>Index of "+ path + "</p>"
+        html += "<ul>"
+        html += files.map((item) => {
+            return "<li><a href='" + item + "'>" + item + "</a></li>"
+        }).join("")
+        html += "</ul>"
+        res.end(html)
+    })
+}
+
+/** @function serverFile 
+ * Serves the specified file with the provided response object
+ * @param {string} path - specifies the file paht to read
+ * @param {serverResponse} res - the http response object
+ */
+
+function serveFile(path,res){
+    fs.readFile(path,(err,data) => {
+        if(err){
+            console.error(data)
+            res.statusCode = 500
+            res.end("Server Error: Could not read file")
+            return
+        }
+        res.end(data)
+            })
+}
+
+/** @function handleRequest
+ * Request handler for our http server
+ * @param {http.ClientRequest} req - the http request object 
+ * @param {http.ClientResponse} res - the http response object
+ */
+
 function handleRequest(req, res) {
-    console.log(req.url);
-    
+    //Map request urls     
     switch(req.url) {
         case '/':
+            serveIndex('public',res)
+            break
         case '/openhouse.html':
-            res.end(fs.readFileSync('public/openhouse.html'));
+            serveFile('public/openhouse.html',res)
             break;
         case '/openhouse.css':
-            res.end(fs.readFileSync('public/openhouse.css'));
+            serveFile('public/openhouse.css',res)
             break;
         case '/openhouse.js':
-            res.end(fs.readFileSync('public/openhouse.js'));
+            serveFile('public/openhouse.js',res)
             break;
         default:
             res.statusCode = 404;
